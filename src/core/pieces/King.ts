c64a99e61a1d58d6f2c6bb00f242676d9cf767be
J-Romulo <joseromulo.10@hotmail.com>
2025-07-01 19:21:49 +1200

    feat: add castling logic in rook and king representations

--- src/core/pieces/King.ts
+++ src/core/pieces/King.ts
@@ -1,16 +1,18 @@
 import { Board } from "../board/Board";
-import { Piece } from "./Piece";
+import { Movement, Piece } from "./Piece";
 
 export class King extends Piece {
     constructor(color: 'black' | 'white') {
         super(color, {
             column: 4,
             row: color === 'black' ? 7 : 0
-        })
+        }, 'K')
     }
 
     validMovements(board: Board) {
         const validMovements = [];
+        const currentRow = this.position.row;
+        const currentColumn = this.position.column;
 
         const kingMoves = [
             { row: 1, column: 1 }, // Right - top
@@ -31,14 +33,71 @@ export class King extends Piece {
             let row = this.position.row + rowDir;
             let col = this.position.column + colDir;
 
-            if(row > 7 || row < 0 || col > 7 || col < 0) return
+            if(row > 7 || row < 0 || col > 7 || col < 0) continue;
             const possibleSquare = board.getSquare({ row, column: col });
             
             if (possibleSquare.empty) {
-                validMovements.push({ row, column: col });
+                validMovements.push({ row, column: col, type: 'move' as Movement['type'] });
             } else {
                 if (possibleSquare.piece!.color !== this.color) {
-                    validMovements.push({ row, column: col });
+                    validMovements.push({ row, column: col, type: 'capture' as Movement['type'] });
+                }
+            }
+        }
+
+        //King castling
+        if(this.movementsMade === 0) {
+            const castlingSquares = [
+                { column: currentColumn + 1, row: currentRow },
+                { column: currentColumn + 2, row: currentRow }
+            ]
+
+            let castlingPossible = true
+            for (const move of castlingSquares) {
+                if(move.column > 7 || move.column < 0) {
+                    castlingPossible
+                    break
+                }
+                const targetSquare = board.getSquare(move);
+                if(!targetSquare.empty) {
+                    castlingPossible = false;
+                    break;
+                }
+            }
+
+            if(castlingPossible) {
+                const rookSquare = board.getSquare({ column: currentColumn + 3, row: currentRow });
+                if(!rookSquare.empty && rookSquare.piece!.color === this.color && rookSquare.piece!.movementsMade === 0) {
+                    validMovements.push({ column: currentColumn + 2, row: currentRow, type: 'king_castling' as Movement['type'] });
+                }
+            }
+        }
+
+        //Queen castling
+        if(this.movementsMade === 0) {
+            const castlingSquares = [
+                { column: currentColumn - 1, row: currentRow },
+                { column: currentColumn - 2, row: currentRow },
+                { column: currentColumn - 3, row: currentRow },
+            ]
+
+            let castlingPossible = true
+            for (const move of castlingSquares) {
+                if(move.column > 7 || move.column < 0){
+                    castlingPossible = false;
+                    break;
+                }
+                const targetSquare = board.getSquare(move);
+                if(!targetSquare.empty) {
+                    castlingPossible = false;
+                    break;
+                }
+            }
+
+            if(castlingPossible) {
+                const rookSquare = board.getSquare({ column: currentColumn - 4, row: currentRow });
+                if(!rookSquare.empty && rookSquare.piece!.color === this.color && rookSquare.piece!.movementsMade === 0) {
+                    validMovements.push({ column: currentColumn - 2, row: currentRow, type: 'queen_castling' as Movement['type'] });
                 }
             }
         }
--- src/core/pieces/Rook.ts
+++ src/core/pieces/Rook.ts
@@ -1,16 +1,18 @@
 import { Board } from "../board/Board";
-import { Piece } from "./Piece"
+import { Movement, Piece } from "./Piece"
 
 export class Rook extends Piece {
     constructor(color: 'black' | 'white', type: 1 | 2) {
         super(color, {
             column: type === 1 ? 0 : 7,
             row: color === 'black' ? 7 : 0
-        })
+        }, 'R')
     }
 
     validMovements(board: Board) {
         const validMovements = [];
+        const currentRow = this.position.row;
+        const currentColumn = this.position.column;
 
         const directions = [
             { row: 0, column: 1 }, // Right
@@ -27,16 +29,73 @@ export class Rook extends Piece {
                 const possibleSquare = board.getSquare({ row, column: col });
                 
                 if (possibleSquare.empty) {
-                    validMovements.push({ row, column: col });
+                    validMovements.push({ row, column: col, type: 'move' as Movement['type'] });
                 } else {
                     if (possibleSquare.piece!.color !== this.color) {
-                        validMovements.push({ row, column: col });
+                        validMovements.push({ row, column: col, type: 'capture' as Movement['type'] });
                     }
                     break;
                 }
 
                 row += rowDir;
                 col += colDir;
+            }
+        }
+
+        //King castling
+        if(this.movementsMade === 0) {
+            const castlingSquares = [
+                { column: currentColumn - 1, row: currentRow },
+                { column: currentColumn - 2, row: currentRow }
+            ]
+
+            let castlingPossible = true
+            for (const move of castlingSquares) {
+                if(move.column > 7 || move.column < 0){
+                    castlingPossible = false 
+                    break
+                }
+                const targetSquare = board.getSquare(move);
+                if(!targetSquare.empty) {
+                    castlingPossible = false;
+                    break;
+                }
+            }
+
+            if(castlingPossible) {
+                const kingSquare = board.getSquare({ column: currentColumn - 3, row: currentRow });
+                if(!kingSquare.empty && kingSquare.piece!.color === this.color && kingSquare.piece!.movementsMade === 0) {
+                    validMovements.push({ column: currentColumn - 2, row: currentRow, type: 'king_castling' as Movement['type'] });
+                }
+            }
+        }
+
+        //Queen castling
+        if(this.movementsMade === 0) {
+            const castlingSquares = [
+                { column: currentColumn + 1, row: currentRow },
+                { column: currentColumn + 2, row: currentRow },
+                { column: currentColumn + 3, row: currentRow },
+            ]
+
+            let castlingPossible = true
+            for (const move of castlingSquares) {
+                if(move.column > 7 || move.column < 0){ 
+                    castlingPossible = false 
+                    break
+                }
+                const targetSquare = board.getSquare(move);
+                if(!targetSquare.empty) {
+                    castlingPossible = false;
+                    break;
+                }
+            }
+
+            if(castlingPossible) {
+                const kingSquare = board.getSquare({ column: currentColumn + 4, row: currentRow });
+                if(!kingSquare.empty && kingSquare.piece!.color === this.color && kingSquare.piece!.movementsMade === 0) {
+                    validMovements.push({ column: currentColumn + 3, row: currentRow, type: 'queen_castling' as Movement['type'] });
+                }
             }
         }
 
