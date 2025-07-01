dfd78c13483ab6ab5998b59485c9c44aa46785f7
J-Romulo <joseromulo.10@hotmail.com>
2025-07-01 19:22:21 +1200

    feat: add the en passant logic in pawn representation

--- src/core/pieces/Pawn.ts
+++ src/core/pieces/Pawn.ts
@@ -1,9 +1,7 @@
 import { Board } from "../board/Board"
-import { Piece } from "./Piece"
+import { Piece, Movement } from "./Piece"
 
 export class Pawn extends Piece {
-    firstMove = true
-    // enpassant
     constructor(color: 'black' | 'white', column: number) {
         super(color, {
             column,
@@ -12,23 +10,23 @@ export class Pawn extends Piece {
     }
 
     validMovements(board: Board) {
-        const validMovements = [];
+        const validMovements: Movement[] = [];
         const direction = this.color === 'white' ? 1 : -1;
         const currentRow = this.position.row;
         const currentColumn = this.position.column;
 
         // Move one square forward
-        const oneSquare = { column: currentColumn, row: currentRow + direction };
+        const oneSquare = { column: currentColumn, row: currentRow + direction, type: 'move' as Movement['type'] };
 
         if(oneSquare.row > 7 || oneSquare.row < 0) return
         
-        if (board.getSquare(oneSquare).empty) {
+        if ((oneSquare.row <= 7 && oneSquare.row >= 0) && board.getSquare(oneSquare).empty) {
             validMovements.push(oneSquare);
         }
 
         // Move two squares forward on first move
-        if (this.firstMove) {
-            const twoSquares = { column: currentColumn, row: currentRow + 2 * direction };
+        if (this.movementsMade === 0) {
+            const twoSquares = { column: currentColumn, row: currentRow + 2 * direction, type: 'move' as Movement['type'] };
             if (board.getSquare(oneSquare).empty && board.getSquare(twoSquares).empty) {
                 validMovements.push(twoSquares);
             }
@@ -36,15 +34,42 @@ export class Pawn extends Piece {
 
         // Capture diagonally
         const diagonalMoves = [
-            { column: currentColumn + 1, row: currentRow + direction },
-            { column: currentColumn - 1, row: currentRow + direction }
+            { column: currentColumn + 1, row: currentRow + direction, type: 'capture' as Movement['type'] },
+            { column: currentColumn - 1, row: currentRow + direction, type: 'capture' as Movement['type'] }
         ];
 
         for (const move of diagonalMoves) {
-            if(move.column > 7 || move.column < 0) return
+            if(move.column > 7 || move.column < 0) break
             const targetSquare = board.getSquare(move);
             if (!targetSquare.empty && targetSquare.piece!.color !== this.color) {
                 validMovements.push(move);
+            }
+        }
+
+        // En passant
+        // Capturing pawn has to have moved 3 rows
+        // Captured pawn has to be next to the capturing pawn
+        // Captured pawn has have moved 2 rows in last turn
+        // Capture has to be made immediately after the pawn has moved 2 rows
+        if(this.movementsMade === 3) {
+            const sideSquares = [
+                { column: currentColumn + 1, row: currentRow },
+                { column: currentColumn - 1, row: currentRow }
+            ]
+    
+            for (const move of sideSquares) {
+                if(move.column > 7 || move.column < 0) break
+                const targetSquare = board.getSquare(move);
+                if (!targetSquare.empty && targetSquare.piece!.color !== this.color && targetSquare.piece instanceof Pawn) {
+                    if(
+                        targetSquare.piece.lastPosition && 
+                        targetSquare.piece.movementsMade === 2 &&
+                        Math.abs(targetSquare.piece.position.row - targetSquare.piece.lastPosition.row) === 2 &&
+                        targetSquare.piece.lastPosition.round === board.round - 1
+                    ) {
+                        validMovements.push({ column: move.column, row: currentRow + direction, type: 'en-passant' as Movement['type'] });
+                    }
+                }
             }
         }
 
